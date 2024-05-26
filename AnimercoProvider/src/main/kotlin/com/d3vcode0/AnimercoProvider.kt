3221102769caf0ea2f0c41c2b25b8ca18443ec7e
devosto <gamosgame2@gmail.com>
2024-05-26 00:06:15 +0000

    Update AnimercoProvider.kt

--- AnimercoProvider/src/main/kotlin/com/d3vcode0/AnimercoProvider.kt
+++ AnimercoProvider/src/main/kotlin/com/d3vcode0/AnimercoProvider.kt
@@ -14,14 +14,15 @@ class AnimercoProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.AnimeMovie, TvType.Anime)
 
     override val mainPage = mainPageOf(
-        "$mainUrl/animes/" to "Animes",
-        "$mainUrl/seasons/" to "Seasons",
-        "$mainUrl/episodes/" to "Episodes",
-        "$mainUrl/schedule/" to "Schedule",
+        "$mainUrl/animes/" to "قائمة الأنمي",
+        "$mainUrl/movies/" to "قائمة الأفلام",
+        "$mainUrl/seasons/" to "قائمة المواسم",
+        "$mainUrl/episodes/" to "قائمة الحلقات ",
+        "$mainUrl/schedule/" to "يعرض اليوم",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val regex = Regex("animes|seasons")
+        val regex = Regex("animes|seasons|movies")
         val isTrue = regex.containsMatchIn(request.data)
         val home = if(isTrue){
 
@@ -57,20 +58,61 @@ class AnimercoProvider : MainAPI() {
             HomePageList(request.name, list, false)
 
         }
-        return newHomePageResponse(home)
+        return newHomePageResponse(home, hasNext = !request.name("Schedule"))
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+        val titleJap = document.selectFirst("div.media-title h1")?.text()?.trim() ?: return null
+        val titleEng = document.selectFirst("div.media-title h3")?.text()?.trim() ?: return null
+        val posterUrlBg = document.selectFirst("div.head-box div.banner")?.attr("data-src") ?: return null
+        val posterUrl = document.selectFirst("div.anime-card .image")?.attr("data-src") ?: return null
+        val tags = document.select("div.genres a").mapNotNull{ it?.text()?.trim() }
+        val plot = document.selectFirst("div.content p")?.text()?.trim() ?: return null
+        val tv = document.selectFirst("ul.media-info li:contains(النوع:) span")?.text()
+        val tvType = if(tv.contains("Movie")) TvType.AnimeMovie else TvType.Anime
+        val status = document.selectFirst("div.status")?.text()?.trim()
+        val showStatus = if(status.contains("مكتمل")) ShowStatus.Completed else if(status.contains("يعرض الأن")) ShowStatus.Ongoing else null
+
+        val regex = Regex("animes|seasons|movies")
+        val txt = url.split("/")[3]
+        if(txt) {
+            //Animes & Seasons
+            //TODO: add episodes
+            newAnimeLoadResponse(titleEng, url, TvType.Anime, true) {
+                this.engName = titleEng
+                this.japName = titleJap
+                this.posterUrl = posterUrl
+                this.plot = plot
+                this.tags = tags
+                this.showStatus = showStatus
+                this.backgroundPosterUrl = posterUrlBg
+            }
+        } else {
+            //Movies & Episodes
+            //TODO: Add more
+            newMovieLoadResponse(titleEng, url, TvType.Movie, url){
+                this.posterUrl = posterUrl
+                this.plot = plot
+                this.tags = tags
+                this.backgroundPosterUrl = posterUrlBg
+            }
+        }
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
         val title = this.selectFirst("div.info h3")?.text()?.trim() ?: return null
         val href = this.selectFirst("a")?.attr("href") ?: return null
         val posterUrl = this.selectFirst("a")?.attr("data-src") ?: return null
+        val e = this.selectFirst("a.episode")?.text()?.trim()?.replace("الحلقة ", "") ?: return null
+        val s = this.selectFirst("a.extra")?.text()?.trim()?.replace("الموسم ", "") ?: return null
 
         return if(href.contains("movies")) {
             newMovieSearchResponse(title, href, TvType.AnimeMovie) {
                 this.posterUrl = posterUrl
             }
         } else {
-            newAnimeSearchResponse(title, href, TvType.Anime) {
+            newAnimeSearchResponse("$title Se-$e", href, TvType.Anime) {
                 this.posterUrl = posterUrl
             }
         }
